a0c9ba25caf5b603d8e5b96eba238bc942d95ade
Rajitha-Kingston <93071849+Rajitha-Kingston@users.noreply.github.com>
2022-05-05 21:58:27 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,52 @@
+const products = [
+	{
+		name: 'Surf Wave Wooden Box',
+		image: '/images/surfwavewoodenbox.jpg',
+		description:
+			'The wooden box that caught the surf wave.',
+		brand: 'Wooden',
+		category: 'Box',
+		price: 199.99,
+		countInStock: 5,
+		rating: 0,
+		numReviews: 0,
+	},
+	{
+		name: 'Wooden Kettle',
+		image: '/images/woodenkettle.jpg',
+		description:
+			'Wooden kettle to boil water to make some tea.',
+		brand: 'Wooden',
+		category: 'Kettle',
+		price: 119.99,
+		countInStock: 8,
+		rating: 0,
+		numReviews: 0,
+	},
+	{
+		name: 'Tunnel Wooden Box',
+		image: '/images/tunnelwoodenbox.jpg',
+		description:
+			'Tunnel shaped wooden jewellery box.',
+		brand: 'Wooden',
+		category: 'Box',
+		price: 199.99,
+		countInStock: 6,
+		rating: 0,
+		numReviews: 0,
+	},
+	{
+		name: 'Large Decorated Designer Wooden Box ',
+		image: '/images/largedesignbox.jpg',
+		description:
+			'Large wooden box with a designer pattern.',
+		brand: 'Wooden',
+		category: 'Box',
+		price: 99.99,
+		countInStock: 0,
+		rating: 0,
+		numReviews: 0,
+	}
+]
+
+export default products
